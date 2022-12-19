5e8b5fb2b87ea806517d6af90a9008f86845f8a4
seele <liuyifan115@hotmail.com>
2022-12-20 00:16:08 +0800

    dev

--- main.c
+++ main.c
@@ -18,12 +18,29 @@ typedef struct {
     unsigned long long lockdownCost; //封城的花费
 }DATA;
 
-//这边是各种全局变量
-double what_this = 3; //这是什么鬼？
-double infectRatio = 3; //感染率，比如说一个人一天能传播三个人，那么感染率就是3
-int ifLockdown = 0; //是否会封城
-unsigned long lockdownLimit = 0; //感染到多少人就会封城
-int lockedDown = 0; //是否已经封城
+//模拟相关状态
+struct { //基础状态
+    int days;//模拟天数
+    DATA *data; //存放数据的地址
+} globe = {30,NULL};
+
+struct { //感染状态
+    double what_this;//这是什么鬼？
+    double infectRatio;//感染率，比如说一个人一天能传播三个人，那么感染率就是3
+    int ifInfectAgain;//能否被再次感染
+    int daysInfectedAgain;//治愈几天后能再次被感染
+}infect = {3,3,0,0};
+
+struct { //防治措施状态
+    int ifLockdown;//是否会封城
+    unsigned long lockdownLimit;//感染到多少人就会封城
+    int ifLockedDown;//是否已经封城
+    int ifAllSendToShelter;//每个感染者是否都去方舱
+    double rationInfectedToShelter;//感染者去方舱的比例
+    double infectRationInHome;//居家和封城时的感染率
+} prevent = {0,1000000,0,1,1,0.1};
+
+
 SHELTER shelter[3000] = {};//一共有3,000,000人，一个方舱能住1000人，所以3000个足矣
 DATA day0 = {3000000, 0, 0, 0, 0,0,0};//这是初始的数据
 DATA data[30] = {};//这是30天，按每一天存放的数据
@@ -40,38 +57,70 @@ void whatIWant();//这又是什么鬼？
 int main(){
     //创建目录来存放输出文件
     system("mkdir output");
+
+    //设定模拟天数
+    printf("How many days will the simulation last:");
+    scanf("%d",&globe.days);
+
+    globe.data = malloc(globe.days * sizeof(DATA));
+
     //自定义感染率
     double ration;
     printf("Set your infect ration:");
     scanf("%lf",&ration);
 
+    //感染之后是否会再次被感染
+    printf("Will the cured be infected again (0-false 1-true):");
+    scanf("%d",&infect.ifInfectAgain);
+
+    //如果会再次感染，那么几天后能被再次感染
+    if (prevent.ifLockdown){
+        printf("When to be infected again:");
+        scanf("%d",&infect.daysInfectedAgain);
+    }
+
+    //是否所有感染者都去方舱
+    printf("Will all the infected be sent to shelter (0-false 1-true):");
+    scanf("%d",&prevent.ifAllSendToShelter);
+
+    //如果不是，设置去方舱的感染者比例和居家隔离的感染率
+    if (!prevent.ifAllSendToShelter){
+        printf("Set the ration of the infected sent to shelter:");
+        scanf("%lf",&prevent.rationInfectedToShelter);
+
+        printf("Set the infect ration in home or lockdown:");
+        scanf("%lf",&prevent.infectRationInHome);
+    }
+
     //选择是否会封城
     printf("If lockdown (0-false 1-true):");
-    scanf("%d",&ifLockdown);
+    scanf("%d",&prevent.ifLockdown);
 
     //如果会封城，那么感染到多少人会封城
-    if (ifLockdown == 1){
+    if (prevent.ifLockdown){
         printf("When to lockdown:");
-        scanf("%lu",&lockdownLimit);
+        scanf("%lu",&prevent.lockdownLimit);
+
+
     }
     //保存自定义数据
-    what_this = ration;
-    infectRatio = ration;
+    infect.what_this = ration;
+    infect.infectRatio = ration;
 
     //初始化数据
     dataInit();
 
     //控制台输出，用于调试
     printf("%-12s%-12s%-12s%-12s%-12s%-12s%-12s%-12s\n","Days","Health","Infected","InShelter","NotInShelter","Cured","Cost","LockdownCost");
-    printf("%-12d%-12lu%-12lu%-12lu%-12lu%-12lu%-12llu%-12llu\n",1,data[0].health,data[0].infected,data[0].inShelter,data[0].notInShelter,data[0].cured,data[0].cost,data[0].lockdownCost);
+    printf("%-12d%-12lu%-12lu%-12lu%-12lu%-12lu%-12llu%-12llu\n",1,globe.data[0].health,globe.data[0].infected,globe.data[0].inShelter,globe.data[0].notInShelter,globe.data[0].cured,globe.data[0].cost,globe.data[0].lockdownCost);
 
     writeData(0);
     //用循环模拟30天的数据
-    for (int day = 1; day < 30; ++day) {
+    for (int day = 1; day < globe.days; ++day) {
         infectionUpdate(day);
         shelterUpdate(day);
         infectRatioUpdate(day);
-        printf("%-12d%-12lu%-12lu%-12lu%-12lu%-12lu%-12llu%-12llu\n",day+1,data[day].health,data[day].infected,data[day].inShelter,data[day].notInShelter,data[day].cured,data[day].cost,data[day].lockdownCost);
+        printf("%-12d%-12lu%-12lu%-12lu%-12lu%-12lu%-12llu%-12llu\n",day+1,globe.data[day].health,globe.data[day].infected,globe.data[day].inShelter,globe.data[day].notInShelter,globe.data[day].cured,globe.data[day].cost,globe.data[day].lockdownCost);
         writeData(day);
     }
     whatIWant();
@@ -81,44 +130,44 @@ int main(){
 
 //初始化数据，其实就是创造一个感染者
 void dataInit(){
-    data[0].health = day0.health - 1;
-    data[0].infected = 1;
-    data[0].notInShelter = 1;
+    globe.data[0].health = day0.health - 1;
+    globe.data[0].infected = 1;
+    globe.data[0].notInShelter = 1;
 }
 
 //进行感染的过程
 void infectionUpdate(int day){
 
     //新感染的人数就等于没有进方舱的感染者乘以感染率
-    unsigned long newInfected = (unsigned long)(data[day-1].notInShelter * infectRatio);
+    unsigned long newInfected = (unsigned long)((double)globe.data[day-1].notInShelter * infect.infectRatio);
 
     //如果新感染的人数超过了剩余健康人数，那么新感染人数就是剩余的健康人数
-    if (data[day-1].health < newInfected){
-        newInfected = data[day-1].health;
+    if (globe.data[day-1].health < newInfected){
+        newInfected = globe.data[day-1].health;
     }
 
     //将相应数据写入到当天的元素中
-    data[day].notInShelter = data[day-1].notInShelter + newInfected;
-    data[day].infected = data[day-1].infected + newInfected;
-    data[day].health = data[day-1].health - newInfected;
-    data[day].cured = data[day-1].cured;
-    data[day].inShelter = data[day-1].inShelter;
-    data[day].cost = data[day-1].cost;
-    data[day].lockdownCost = data[day-1].lockdownCost;
+    globe.data[day].notInShelter = globe.data[day-1].notInShelter + newInfected;
+    globe.data[day].infected = globe.data[day-1].infected + newInfected;
+    globe.data[day].health = globe.data[day-1].health - newInfected;
+    globe.data[day].cured = globe.data[day-1].cured;
+    globe.data[day].inShelter = globe.data[day-1].inShelter;
+    globe.data[day].cost = globe.data[day-1].cost;
+    globe.data[day].lockdownCost = globe.data[day-1].lockdownCost;
 
     //判断是否要封城
-    if (data[day].infected >= lockdownLimit && ifLockdown){
-        lockedDown = 1;
+    if (globe.data[day].infected >= prevent.lockdownLimit && prevent.ifLockdown){
+        prevent.ifLockedDown = 1;
     }
 
     //判断是否要解除封城
-    if (data[day].infected < lockdownLimit && ifLockdown){
-        lockedDown = 0;
+    if (globe.data[day].infected < prevent.lockdownLimit && prevent.ifLockdown){
+        prevent.ifLockedDown = 0;
     }
 
     //如果封城，那么就在封城开支上加上6,000,000
-    if (lockedDown){
-        data[day].lockdownCost += 60000000;
+    if (prevent.ifLockedDown){
+        globe.data[day].lockdownCost += 60000000;
     }
 }
 
@@ -130,9 +179,9 @@ void shelterUpdate(int day){
     }
 
     //将相应数据写入
-    data[day].cured += newCured;
-    data[day].infected -= newCured;
-    data[day].inShelter -= newCured;
+    globe.data[day].cured += newCured;
+    globe.data[day].infected -= newCured;
+    globe.data[day].inShelter -= newCured;
 
     //方舱中病人的累计天数增长一天
     for (int i = 0; i < 3000; ++i) {
@@ -144,9 +193,9 @@ void shelterUpdate(int day){
     }
 
     //因为设置为感染后第二天被送往方舱，所以当天送到方舱的人数应该是上一天未进方舱的感染人数
-    unsigned long newInShelter = data[day-1].notInShelter;
-    data[day].inShelter += newInShelter;
-    data[day].notInShelter -= newInShelter;
+    unsigned long newInShelter = globe.data[day-1].notInShelter;
+    globe.data[day].inShelter += newInShelter;
+    globe.data[day].notInShelter -= newInShelter;
 
     //从第一个方舱开始收治感染者
     for (int i = 0; i < 3000; ++i) {
@@ -173,16 +222,16 @@ void shelterUpdate(int day){
     for (int i = 0; i < 3000; ++i) {
         newCost += shelter[i].total*200;
     }
-    data[day].cost += newCost;
+    globe.data[day].cost += newCost;
 }
 
 void infectRatioUpdate(int day){
     //因为默认感染之后无法再次被感染，而已经被感染的也无法被感染，所以感染率会随着健康人数的下降有所下降
-    infectRatio = what_this * ((double)data[day].health / (double)(data[day].health + data[day].cured + data[day].infected));
+    infect.infectRatio = infect.what_this * ((double)globe.data[day].health / (double)(globe.data[day].health + globe.data[day].cured + globe.data[day].infected));
 
     //如果封城的话，就讲感染率下降到0.1
-    if (lockedDown && ifLockdown){
-        infectRatio = 0.1;
+    if (prevent.ifLockedDown && prevent.ifLockdown){
+        infect.infectRatio = 0.1;
     }
 }
 
@@ -226,7 +275,7 @@ void writeData(int day){
     }
     sprintf(filename,"./output/total_cost_day_%d.txt",day+1); //通过sprintf来切换日期
     fp = fopen(filename,"w");
-    fprintf(fp,"%llu",data[day].cost+data[day].lockdownCost);
+    fprintf(fp,"%llu",globe.data[day].cost+globe.data[day].lockdownCost);
     fclose(fp);
 
     //写入infected_day_x.txt
@@ -235,7 +284,7 @@ void writeData(int day){
     }
     sprintf(filename,"./output/infected_day_%d.txt",day+1); //通过sprintf来切换日期
     fp = fopen(filename,"w");
-    fprintf(fp,"%lu",data[day].infected);
+    fprintf(fp,"%lu",globe.data[day].infected);
     fclose(fp);
 
     //写入cost_of_lock_down_population_day_x.txt
@@ -244,27 +293,27 @@ void writeData(int day){
     }
     sprintf(filename,"./output/cost_of_lock_down_population_day_%d.txt",day+1); //通过sprintf来切换日期
     fp = fopen(filename,"w");
-    fprintf(fp,"%llu",data[day].lockdownCost);
+    fprintf(fp,"%llu",globe.data[day].lockdownCost);
     fclose(fp);
 }
 
 void whatIWant(){
     FILE *fp = fopen("./output/I_want_this.csv","w");
-    for (int i = 0; i < 29; ++i) {
+    for (int i = 0; i < globe.days-1; ++i) {
         fprintf(fp,"%d,",i+1);
     }
-    fprintf(fp,"%d\n",30);
-    for (int i = 0; i < 29; ++i) {
-        fprintf(fp,"%lu,",data[i].infected);
+    fprintf(fp,"%d\n",globe.days);
+    for (int i = 0; i < globe.days-1; ++i) {
+        fprintf(fp,"%lu,",globe.data[i].infected);
     }
-    fprintf(fp,"%lu\n",data[29].infected);
-    for (int i = 0; i < 29; ++i) {
-        fprintf(fp,"%lu,",data[i].inShelter);
+    fprintf(fp,"%lu\n",globe.data[globe.days-1].infected);
+    for (int i = 0; i < globe.days-1; ++i) {
+        fprintf(fp,"%lu,",globe.data[i].inShelter);
     }
-    fprintf(fp,"%lu\n",data[29].inShelter);
-    for (int i = 0; i < 29; ++i) {
-        fprintf(fp,"%llu,",data[i].cost+data[i].lockdownCost);
+    fprintf(fp,"%lu\n",globe.data[globe.days-1].inShelter);
+    for (int i = 0; i < globe.days-1; ++i) {
+        fprintf(fp,"%llu,",globe.data[i].cost+globe.data[i].lockdownCost);
     }
-    fprintf(fp,"%llu\n",data[29].cost+data[29].lockdownCost);
+    fprintf(fp,"%llu\n",globe.data[globe.days-1].cost+globe.data[globe.days-1].lockdownCost);
     fclose(fp);
 }
